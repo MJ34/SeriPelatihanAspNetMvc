9b78448fdb7c205782b742fa3329a4f1e2f4b575
SkyM94 <kasur.rusak404@gmail.com>
2024-07-11 14:49:29 +0700

    Adding Student Model

---
+++ SeriMvcAsp/Models/StudentModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SeriMvcAsp.Models
+{
+    public class StudentModel
+    {
+        string Name { get; set; }
+        string Email { get; set; }
+        string Password { get; set; }
+        string Confirm_Password { get; set; }
+    }
+}
